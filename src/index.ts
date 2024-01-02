2beda165f9495b2e2d4dec3f35d6e3956fd7a49c
ZOOI <zooii.kut@gmail.com>
2024-01-02 15:50:30 -0500

    :sparkles: Add step, smoothstep & distanceManhattan

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,10 @@ export function distance3D(point1: { x: number, y: number, z: number }, point2: 
     return Math.sqrt(a * a + b * b + c * c);
 }
 
+export function distanceManhattan(point1: { x: number, y: number }, point2: { x: number, y: number }): number {
+    return Math.abs(point1.x - point2.x) + Math.abs(point1.y - point2.y);
+}
+
 export function lerp(x: number, y: number, a: number): number {
     return x * (1 - a) + y * a;
 }
@@ -25,4 +29,13 @@ export function clamp(a: number, min = 0, max = 1): number {
 
 export function range(a: number, x1: number, y1: number, x2: number, y2: number): number {
     return lerp(x2, y2, invlerp(x1, y1, a));
+}
+
+export function step(value: number, x: number): number {
+    return value < x ? 0 : 1;
+}
+
+export function smoothstep(min: number, max: number, value: number): number {
+    var x = Math.max(0, Math.min(1, (value-min) / (max-min)));
+    return x * x * (3 - 2 * x);
 }
