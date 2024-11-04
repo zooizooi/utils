cba3cd232c948580a0210e9cf0d20aaa2d978ca2
zooizooi <mathijspb@gmail.com>
2024-11-04 20:53:12 +0100

    :wrench: Add eslint config

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,6 @@ export function step(value: number, x: number): number {
 }
 
 export function smoothstep(min: number, max: number, value: number): number {
-    var x = Math.max(0, Math.min(1, (value-min) / (max-min)));
+    const x = Math.max(0, Math.min(1, (value - min) / (max - min)));
     return x * x * (3 - 2 * x);
 }
